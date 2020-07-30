c54ad46108b09bbf9744194e14910de2adae6f0c
Caleb Beery <cjbeery@gmail.com>
2020-07-29 22:57:09 -0700

    Finished coding challenge.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,133 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col v-for="category in categories" :key="category">
+        <h3>{{ category }}</h3>
+        <div v-for="(field, key) in getCategoryFields(category)" :key="key" class="d-flex">
+          <div class="flex-grow-1">
+            <v-tooltip top>
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                        :label="field.label"
+                        :type="field.type"
+                        :readonly="field.readOnly"
+                        :value="getConstantValue(key)"
+                        @change="updateValue($event, key)"
+                        v-bind="attrs"
+                        v-on="on"
+                ></v-text-field>
+              </template>
+              <span>{{ field.label }} : {{ getConstantValue(key) }}</span>
+            </v-tooltip>
+          </div>
+          <v-tooltip v-if="getChanges(key) && getChanges(key).change !== 0" top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-icon class="align-self-start"
+                      :class="{'red--text': getChanges(key).change > 0, 'green--text': getChanges(key).change < 0}"
+                      v-bind="attrs"
+                      v-on="on"
+              >{{ getChanges(key).change > 0 ? 'mdi-arrow-up' : 'mdi-arrow-down'}}</v-icon>
+            </template>
+            <span>{{ Math.abs(getChanges(key).change).toFixed(2) }}</span>
+          </v-tooltip>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  import constantsData from './constants.json';
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  function getComputed() {
+    let computed = {};
+    Object.keys(constantsData.fields).forEach(eachField => {
+      if (constantsData.fields[eachField].calculate) {
+        let computeString = constantsData.fields[eachField].calculate;
+        computed[eachField] = () => {
+          return eval(computeString);
+        }
+      }
+    });
+    return computed;
+  }
+
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+      initialized: false,
+      constants: {},
+      categories: [],
+      changes: [],
+    }),
+
+    computed: getComputed(),
+
+    mounted() {
+      this.processConstants();
+    },
+
+    methods: {
+      processConstants() {
+        let vm = this;
+        Object.keys(constantsData.fields).map(eachField => {
+          if (this.categories.indexOf(constantsData.fields[eachField].category) === -1) {
+            this.categories.push(constantsData.fields[eachField].category);
+          }
+          if (constantsData.fields[eachField].calculate) {
+            this.$watch(eachField, function(newValue, oldValue) {
+              if (vm.initialized) {
+                this.changes.push({
+                  field: eachField,
+                  change: newValue - oldValue
+                });
+              }
+            });
+          } else {
+            this.$set(this.constants, eachField, constantsData.fields[eachField].defaultValue);
+            this.$watch('constants.'+eachField, function(newValue, oldValue) {
+              if (vm.initialized) {
+                this.changes.push({
+                  field: eachField,
+                  change: newValue - oldValue
+                });
+              }
+            });
+          }
+        });
+        this.$nextTick(() => {
+          this.initialized = true;
+        })
+      },
+
+      getCategoryFields(category) {
+        let categoryFields = {};
+        Object.keys(constantsData.fields).filter(eachField => {
+          return constantsData.fields[eachField].category === category;
+        }).forEach(eachField => {
+          categoryFields[eachField] = constantsData.fields[eachField];
+        });
+        return categoryFields;
+      },
+
+      getConstantValue(key) {
+        let field = constantsData.fields[key];
+        if (field.calculate) {
+          return Number.isInteger(this[key]*1) ? this[key] : (this[key]*1).toFixed(2);
+        } else {
+          return Number.isInteger(this.constants[key]*1) ? this.constants[key] : (this.constants[key]*1).toFixed(2);
+        }
+      },
+
+      updateValue(changedValue, key) {
+        this.changes = [];
+        this.constants[key] = changedValue;
+      },
+
+      getChanges(key) {
+        return this.changes.find(eachChange => eachChange.field === key);
+      }
+    },
+  }
+</script>
